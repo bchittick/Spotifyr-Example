57067d5313e1c8d19540ec9d1d71d24a35e69884
bchittick <ben_Chittick@yahoo.com>
2020-03-15 11:19:39 -0500

    getting access and what not

--- Spotifyr Example.R
+++ Spotifyr Example.R
@@ -4,11 +4,8 @@
 library(spotifyr)
 library(devtools)
 
+# dev version of the package 
 install_github('charlie86/spotifyr')
-
-Sys.getenv("R_LIBS_USER")
-
-setwd("~/Spotify Side Project")
 
 ### Creds 
 Sys.setenv(SPOTIFY_CLIENT_ID = 'b665cb52a2e64fbaad682eb43fbb203c',
@@ -19,17 +16,15 @@ access_token <- get_spotify_access_token()
 ### Data
 beatles <- get_artist_audio_features('the beatles')
 
-my_id <- 'your spotify id'
-my_plists <- get_user_playlists(my_id)
-
-my_plists2 <- my_plists %>%
-  filter(playlist_name %in% c('Taiwan Top 50', 'France Top 50', 'Bolivia Top 50', 'U.S. Top 50'))
-
-tracks <- get_playlist_tracks(my_plists2)
-features <- get_track_audio_features(tracks)
-
-
-get_user_playlists()
-
+# need this to get one's own data
 get_spotify_authorization_code()
-install.packages("spotifyr")
+
+spotifyr::
+
+
+######################################
+#   General Exploration 
+######################################
+
+t = spotifyr::search_spotify("Kendrick Lamar")
+glimpse(t)
